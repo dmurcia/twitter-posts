b79560a41528beee73c74cdd742e7273401b4734
David Ocampo <dmurcia88@gmail.com>
2022-10-03 18:50:34 -0600

    Finishing lint and test configuration

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// import logo from './logo.svg';
+import './App.css'
+
+function App() {
+  return <div>Prueba</div>
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  test: { // config the vitest
+    environment: 'happy-dom',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+  }
+})
