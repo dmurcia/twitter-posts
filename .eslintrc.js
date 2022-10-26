7ab41789dd8524fef58576c3df0baa7d796f3ec2
David Ocampo <dmurcia88@gmail.com>
2022-10-26 14:05:24 -0600

    new conf with prettier, eslint and lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,16 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   extends: ['plugin:react/recommended', 'standard', 'prettier'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react'],
   rules: {
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/space-before-function-paren': 'off',
-    'comma-dangle': ['error', 'never']
-  }
+    'space-before-function-paren': ['error', 'never'],
+  },
 }
