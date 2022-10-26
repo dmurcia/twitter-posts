ae2cd2fec37cdda9352edd889e00b361cae77e44
David Ocampo <dmurcia88@gmail.com>
2022-10-26 14:33:03 -0600

    conf lint-staged and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,21 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    es2021: true,
     node: true,
+    browser: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   extends: ['plugin:react/recommended', 'standard', 'prettier'],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react'],
   rules: {
-    'space-before-function-paren': ['error', 'never'],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-handler-names': 'off',
+    'space-before-function-paren': 'off',
   },
 }
