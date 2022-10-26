925344a815e50b0e1288f5df65b9abe5ab61e2c7
David Ocampo <dmurcia88@gmail.com>
2022-10-26 13:15:56 -0600

    check conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,19 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: ['plugin:react/recommended', 'standard', 'prettier'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react'],
   rules: {
-    'react/prop-types': ['error', 'never'],
-    'react/react-in-jsx-scope': ['error', 'never'],
-    'react/space-before-function-paren': ['error', 'never'],
-    'comma-dangle': ['error', 'never'],
-  },
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/space-before-function-paren': 'off',
+    'comma-dangle': ['error', 'never']
+  }
 }
