7b7bb3a610622424eebaaa27a12d18afb7daec2d
David Ocampo <dmurcia88@gmail.com>
2022-10-27 22:40:03 -0600

    Styled login

--- src/firebase/client.js
+++ src/firebase/client.js
@@ -7,34 +7,32 @@ import { firebaseConfig } from './config'
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 
-export const loginWithGithub = async () => {
-  const githubProvider = new GithubAuthProvider()
+export const loginWithGithub = () => {
+  return new Promise((resolve, reject) => {
+    const githubProvider = new GithubAuthProvider()
 
-  signInWithPopup(auth, githubProvider)
-    .then((result) => {
-      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-      const credential = GithubAuthProvider.credentialFromResult(result)
-      const token = credential.accessToken
-      if (token) {
-        globalThis.localStorage.setItem('@token', token)
-      }
-
-      // The signed-in user info.
-      const user = result.user
-      console.log(user)
-      // ...
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code
-      const errorMessage = error.message
-      // The email of the user's account used.
-      const email = error.customData.email
-      // The AuthCredential type that was used.
-      const credential = GithubAuthProvider.credentialFromError(error)
-      console.log('error :>> ', errorCode, errorMessage, email, credential)
-      // ...
-    })
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        const credential = GithubAuthProvider.credentialFromResult(result)
+        const token = credential.accessToken
+        if (token) {
+          globalThis.localStorage.setItem('@token', token)
+        }
+        // The signed-in user info.
+        resolve(result.user)
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code
+        const errorMessage = error.message
+        // The email of the user's account used.
+        const email = error.customData.email
+        // The AuthCredential type that was used.
+        const credential = GithubAuthProvider.credentialFromError(error)
+        reject(errorCode, errorMessage, email, credential)
+      })
+  })
 }
 
 export const signOutGithub = () => {
